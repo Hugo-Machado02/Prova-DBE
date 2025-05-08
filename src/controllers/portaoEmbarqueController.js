4a461447ee6a9d7b2715fdca8479795ef6867ffd
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-07 23:31:39 -0300

    ✨ feat: Adiciona método de insercao e update

--- src/controllers/portaoEmbarqueController.js
+++ src/controllers/portaoEmbarqueController.js
@@ -8,6 +8,30 @@ module.exports = {
         res.json({ portoes });
     },
 
+    addPortao: async (req, res) => {
+            const data = matchedData(req);
+            data.disponivel = false;
+    
+            if (data.codigo) {
+                const emailCheck = await PortaoEmbarque.findOne({ codigo: data.codigo });
+                if (emailCheck) {
+                    res.json({ error: "o código do portão já existe!" });
+                    return;
+                }
+            }
+    
+            const newPortao = new PortaoEmbarque(data);
+    
+            const operationAdd = await newPortao.save();
+        
+            if (!operationAdd) {
+                res.json({ error: "Cadastro não realizado!" });
+                return;
+            }
+            res.json({ sucess: true });
+        },
+    
+
     editPortao: async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -16,20 +40,25 @@ module.exports = {
         }
         const data = matchedData(req);
         let updateData = {};
+        console.log(data.id);
 
-        if (!data.codigo) {
-            return res.status(400).json({ error: "Código não foi enviado!" });
-          }
+        if (!data.id) {
+            return res.status(400).json({ error: "Id não especificado!" });
+        }
+
+        if (data.codigo) {
+            updateData.codigo = data.codigo;
+        }
 
         if (data.disponivel) {
             updateData.disponivel = data.disponivel;
         }
         const opValid = await PortaoEmbarque.findOneAndUpdate(
-            { codigo: data.codigo },
+            { _id: data.id },
             { $set: updateData }
           );
           if (!opValid) {
-            res.json({ error: "Código não encontrado!" });
+            res.json({ error: "Id não encontrado!" });
             return;
           }
           res.json({ sucess: true });
