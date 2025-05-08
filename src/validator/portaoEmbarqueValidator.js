f4219f3c5494fdccfc56263ea8ff788d70069187
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-07 23:30:56 -0300

    ✨ feat: Adiciona método de insercao na validacao

--- src/validator/portaoEmbarqueValidator.js
+++ src/validator/portaoEmbarqueValidator.js
@@ -1,7 +1,21 @@
 const { checkSchema } = require("express-validator");
 
 module.exports = {
-  editAction: checkSchema({
+  addPortao: checkSchema({
+    codigo: {
+      notEmpty: true,
+      errorMessage: "Codigo não especificado",
+    },
+  }),
+  editPortao: checkSchema({
+    id: {
+      notEmpty: true,
+      errorMessage: "Id não especificado",
+    },
+    codigo: {
+      notEmpty: true,
+      errorMessage: "Codigo não especificado",
+    },
     disponivel: {
       notEmpty: true,
       errorMessage: "disponibilidade não especificada",
