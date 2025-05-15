73726bc50a2e4277ce946a17d6588ec0dcf29062
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-14 21:20:28 -0300

    ✨ feat: Adiciona as Rotas do Passageiros por voo e Status do Passageiro

--- src/routers/passageiroRouters.js
+++ src/routers/passageiroRouters.js
@@ -4,6 +4,8 @@ const passageiroController = require("../controllers/passageiroController");
 const passageiroValidator = require("../validator/passageiroValidator")
 
 router.get("/passageiros", passageiroController.getPassageiros);
+router.get("/:numeroVoo/passageiros/", passageiroController.getPassageirosVoo);
+router.get("/passageiros/status", passageiroController.getStatusPassageiros);
 router.post("/passageiros/add", passageiroValidator.addPassageiro, passageiroController.addPassageiro);
 router.put("/passageiros/edicao", passageiroValidator.editPassageiro, passageiroController.editPassageiro);
 router.delete("/passageiros/del", passageiroValidator.deletePassageiro, passageiroController.deletePassageiro);
