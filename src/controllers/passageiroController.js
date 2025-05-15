3716a585e25596fe0c852dd047db380698682d1b
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-14 21:14:35 -0300

    ✨ feat: Adiciona função de cadastro de Passageitos

--- src/controllers/passageiroController.js
+++ src/controllers/passageiroController.js
@@ -1,102 +1,96 @@
-const mongoose = require("mongoose");
-const PassageiroModel = require("../models/Passageiro");
-const VooModel = require("../models/Voo");
+const PassageiroService = require("../services/PassageiroService");
 const { validationResult, matchedData } = require("express-validator");
-const { addPassageiro } = require("../validator/passageiroValidator");
 
 module.exports = {
-    //Busca todos os passageiros
     getPassageiros: async (req, res) => {
-        let passageiro = await PassageiroModel.find();
-        res.json({ passageiro });
+        const result = await PassageiroService.getPassageiros();
+        if (result) {
+            res.status(200).json({ Passageiros: result });
+        } else {
+            res.status(500).json({ error: result });
+        }
     },
+
+    getStatusPassageiros: async (req, res) => {
+        const result = await PassageiroService.getStatusPassageiros();
+        if (result) {
+            res.status(200).json({ StatusCheckIn: result });
+        } else {
+            res.status(500).json({ error: result });
+        }
+    },
+
     addPassageiro: async (req, res) => {
+        const erros = validationResult(req);
+        if (!erros.isEmpty()) {
+            return res.status(400).json({ error: erros.mapped() });
+        }
+
         const data = matchedData(req);
-        data.statusCheckIn = "Pendente"
+        const result = await PassageiroService.addPassageiro(data);
 
-        if (data.cpf) {
-            const emailCheck = await PassageiroModel.findOne({ cpf: data.cpf });
-            if (emailCheck) {
-                res.json({ error: "CPF já cadastrado!" });
-                return;
-            }
+        if (result) {
+            res.status(200).json(result);
+        } else {
+            res.status(400).json(result);
         }
-        if (data.vooId) {
-            const emailCheck = await VooModel.findOne({ _id: data.vooId });
-            if (!emailCheck) {
-                res.json({ error: "Voo não encontrado!" });
-                return;
-            }
-        }
-
-        const newPassageiro = new PassageiroModel(data);
-
-        const operationAdd = await newPassageiro.save();
-    
-        if (!operationAdd) {
-            res.json({ error: "Cadastro não realizado!" });
-            return;
-        }
-        res.json({ sucess: true });
     },
 
     editPassageiro: async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.json({ error: errors.mapped() });
-            return;
+        const erros = validationResult(req);
+        if (!erros.isEmpty()) {
+            return res.status(400).json({ error: erros.mapped() });
         }
+
         const data = matchedData(req);
-        let updateData = {};
-        console.log(data.id);
 
-        if (data.nome) {
-            updateData.nome = data.nome;
-        }
-        if (data.cpf) {
-            const resultPortao = await VooModel.findOne({ cpf: data.cpf });
-            if (!resultPortao) {
-                res.json({ error: "CPF já cadastrado!" });
-                return;
-            }
-            updateData.vooId = data.vooId;
-            updateData.cpf = data.cpf;
-        }
-        if (data.vooId) {
-            const resultPortao = await VooModel.findOne({ _id: data.vooId });
-            if (!resultPortao) {
-                res.json({ error: "Portão Não encontrado! Por favor especifique outro" });
-                return;
-            }
-            updateData.vooId = data.vooId;
-        }
-        if (data.statusCheckIn) {
-            updateData.statusCheckIn = data.statusCheckIn;
-        }
+        const result = await PassageiroService.editPassageiro(data);
 
-        const opValid = await PassageiroModel.findOneAndUpdate( { _id: data.id }, { $set: updateData });
-        if (!opValid) {
-            res.json({ error: "Id não encontrado!" });
-            return;
+        if (result) {
+            res.json(result);
+        } else {
+            res.status(400).json({ error: result });
         }
-        res.json({ sucess: true });
     },
-    deletePassageiro: async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-        res.json({
-            error: errors.mapped(),
-        });
-        return;
-        }
-    
-        const data = matchedData(req);
 
-        const operation = await PassageiroModel.findByIdAndDelete(data.id)
-        if (!operation) {
-            res.json({ error: "Passageiro não encontrado!" });
-            return;
+    deletePassageiro: async (req, res) => {
+        const erros = validationResult(req);
+            if (!erros.isEmpty()) {
+        return res.status(400).json({ error: erros.mapped() });
         }
-        res.json({ sucess: true });
+
+        const data = matchedData(req)
+
+        const result = await PassageiroService.deletePassageiro(data.id);
+
+        if (result) {
+            res.json(result);
+        } else {
+            res.status(404).json({ error: result });
+        }
+    },
+
+    getPassageirosVoo: async (req, res) => {
+        const { numeroVoo } = req.params;
+        console.log(numeroVoo);
+
+        const result = await PassageiroService.getPassageirosVoo(numeroVoo);
+        if (result) {
+            res.status(200).json({ "Passageiros - Voo" : result });
+        } else {
+            res.status(500).json({ error: result });
+        }
+    },
+
+    checkInPassageiro: async (req, res) => {
+        const data = matchedData(req)
+
+        const result = await PassageiroService.checkInPassageiro(data.id);
+
+        if (result) {
+            res.json(result);
+        } else {
+            res.status(400).json({ error: result });
+        }
     },
 };
