fb7796e894f2c8254498280ca43892dd9912696f
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-14 21:25:19 -0300

    ♻️ refactor: Altera chamada do Portão

--- src/routers/portaoRouters.js
+++ src/routers/portaoRouters.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const portaoController = require("../controllers/portaoEmbarqueController");
+const portaoController = require("../controllers/portaoController");
 const portaoValidador = require("../validator/portaoEmbarqueValidator")
 
 router.get("/portoesEmbarque", portaoController.getPortao);
