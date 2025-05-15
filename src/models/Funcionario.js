6e64f2a665a817ab6fadafd1ee07715bb526f00e
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-14 21:22:49 -0300

    ✨ feat: Implementa Classe dos Funcionários

---
+++ src/models/Funcionario.js
@@ -0,0 +1,19 @@
+
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const modelSchema = new mongoose.Schema({
+    nome: String,
+    email: String,
+    cargo: String,
+    senha: String
+},{ versionKey: false });
+
+const modelName = "funcionarios";
+
+if (mongoose.connection && mongoose.connection.models[modelName]) {
+    module.exports = mongoose.connection.models[modelName];
+} else {
+    module.exports = mongoose.model(modelName, modelSchema);
+}
