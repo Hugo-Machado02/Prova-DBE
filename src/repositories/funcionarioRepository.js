308f3460640d8ef7b73ef3ae12eda6d69e9a719f
GarotosDePr0grama <shayrakellyeleoterio@gmail.com>
2025-05-21 00:04:41 -0300

    :sparkles: feat: Repositórios dos Funcionários

---
+++ src/repositories/funcionarioRepository.js
@@ -0,0 +1,30 @@
+class FuncionarioRepository {
+    async getAll() {
+      return await FuncionarioModel.find();
+    }
+  
+    async getByEmail(email) {
+      return await FuncionarioModel.findOne({ email });
+    }
+  
+    async getBasicInfo() {
+      return await FuncionarioModel.find().select('nome cargo');
+    }
+  
+    async create(data) {
+      const novoFuncionario = new FuncionarioModel(data);
+      return await novoFuncionario.save();
+    }
+  
+    async updateByEmail(email, updateData) {
+      return await FuncionarioModel.findOneAndUpdate(
+        { email },
+        { $set: updateData },
+        { new: true }
+      );
+    }
+  
+    async deleteByEmail(email) {
+      return await FuncionarioModel.findOneAndDelete({ email });
+    }
+  }
