27efd5a77c35ba0f5774266a43f5978f235b9f40
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-21 12:19:40 -0300

    ✨ feat: Implementa rota de login do funcionário

--- src/routers/funcionarioRouters.js
+++ src/routers/funcionarioRouters.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const funcionarioController = require("../controllers/funcionarioController");
+const authController = require("../controllers/authController");
 const funcionarioValidator = require("../validator/funcionarioValidator")
+// const authValidator = require("../validator/funcionarioValidator")
 
 router.get("/funcionarios", funcionarioController.getFuncionarios);
 router.post("/funcionarios/add", funcionarioValidator.addFuncionario, funcionarioController.addFuncionario);
-router.put("/funcionarios/edicao", funcionarioValidator.editFuncionario, funcionarioController.editFuncionario);
-router.delete("/funcionarios/del", funcionarioValidator.deleteFuncionario, funcionarioController.deleteFuncionario);
+router.post("/login", authController.authLogin);
 
 module.exports = router;
