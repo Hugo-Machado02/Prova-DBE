9c91840ba37a1c380ea22d792a95e6d76b515c3f
GarotosDePr0grama <shayrakellyeleoterio@gmail.com>
2025-05-20 23:57:57 -0300

    :sparkles: feat: Model dos Funcionários

--- src/models/Funcionario.js
+++ src/models/Funcionario.js
@@ -1,19 +1,17 @@
-
-
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const modelSchema = new mongoose.Schema({
-    nome: String,
-    email: String,
-    cargo: String,
-    senha: String
-},{ versionKey: false });
+  nome: String,
+  email: { type: String, unique: true},
+  cargo: String,
+  senha: String
+}, { versionKey: false });
 
 const modelName = "funcionarios";
 
 if (mongoose.connection && mongoose.connection.models[modelName]) {
-    module.exports = mongoose.connection.models[modelName];
+  module.exports = mongoose.connection.models[modelName];
 } else {
-    module.exports = mongoose.model(modelName, modelSchema);
+  module.exports = mongoose.model(modelName, modelSchema);
 }
