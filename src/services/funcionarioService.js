43069efb8c8bff9fd74bfa7c4b9026fa924f6300
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-21 12:28:12 -0300

    :wastebasket: remove: Remove funções desnecessárias

--- src/services/funcionarioService.js
+++ src/services/funcionarioService.js
@@ -1,58 +1,25 @@
 const FuncionarioRepository = require("../repositories/FuncionarioRepository");
+const bcrypt = require("bcrypt");
 
 class FuncionarioService {
-    constructor() {
-        this.funcionarioRepository = new FuncionarioRepository();
-    }
     
-    async getFuncionarios() {
-        return await this.funcionarioRepository.getAll();
+    async getAllFuncionarios() {
+        return await FuncionarioRepository.getAll();
     }
     
     async addFuncionario(data) {
-        const funcionarioExistente = await this.funcionarioRepository.getByEmail(data.email);
+        console.log(data)
+        const funcionarioExistente = await FuncionarioRepository.getByEmail(data.email);
         if (funcionarioExistente) {
             return { error: "E-mail já cadastrado" };
         }
 
-        const resultado = await this.funcionarioRepository.create(data);
+        data.password = await bcrypt.hash(data.password, 10);
+        console.log(data.password)
+
+        const resultado = await FuncionarioRepository.create(data)
         if (!resultado) {
             return { error: "Cadastro não realizado" };
-        }
-
-        return { success: true };
-    }
-    
-    async editFuncionario(data) {
-        if (!data.email) {
-            return { error: "E-mail não informado" };
-        }
-
-        const funcionario = await this.funcionarioRepository.getByEmail(data.email);
-        if (!funcionario) {
-            return { error: "Funcionário não encontrado" };
-        }
-
-        const update = {};
-
-        if (data.nome) update.nome = data.nome;
-        if (data.cargo) update.cargo = data.cargo;
-        if (data.senha) update.senha = data.senha;
-
-        const atualizado = await this.funcionarioRepository.updateByEmail(data.email, update);
-
-        if (!atualizado) {
-            return { error: "Erro ao atualizar funcionário" };
-        }
-
-        return { success: true };
-    }
-    
-    async deleteFuncionario(email) {
-        const deletado = await this.funcionarioRepository.deleteByEmail(email);
-
-        if (!deletado) {
-            return { error: "Funcionário não encontrado" };
         }
 
         return { success: true };
--- src/validator/funcionarioValidator.js
+++ src/validator/funcionarioValidator.js
@@ -1,5 +1,4 @@
 const { checkSchema } = require("express-validator");
-const { addFuncionario } = require("../controllers/funcionarioController");
 
 module.exports = {
   addFuncionario: checkSchema({
@@ -12,60 +11,25 @@ module.exports = {
     },
 
     email: {
-      notEmpty: true,
-      isLength: {
-        options: { min: 10},
-      },
-      errorMessage: "O e-mail deve ter, no mínimo, 10 caracteres",
+      isEmail: true,
+      normalizeEmail: true,
+      errorMessage: "email inválido",
     },
-    senha: {
+
+    cargo: {
+      notEmpty: true,
+      errorMessage: "Cargo não especificado",
+
+    },
+
+    password: {
       notEmpty: {
         errorMessage: "A senha é obrigatória"
       },
       isLength:{
-        options: { min: 8},
+        options: { min: 5},
+        errorMessage: "A senha deve ter, no mínimo, 5 caracteres",
       },
-      errorMessage: "A senha deve ter, no mínimo, 8 caracteres",
-    },
-    cargo: {
-      notEmpty: true,
-      errorMessage: "Cargo não especificado",
-    },
-  }),
-  
-  editFuncionario: checkSchema({
-    nome: {
-        notEmpty: true,
-        isLength: {
-          options: { min: 2 },
-        },
-        errorMessage: "O nome do funcionário deve ter, no mínimo, 2 caracteres",
-      },
-      email: {
-        notEmpty: true,
-        isLength: {
-          options: { min: 10},
-        },
-        errorMessage: "O e-mail deve ter, no mínimo, 10 caracteres",
-      },
-      senha: {
-        notEmpty: {
-          errorMessage: "A senha é obrigatória"
-        },
-        isLength:{
-          options: { min: 8},
-        },
-        errorMessage: "A senha deve ter, no mínimo, 8 caracteres",
-      },
-      cargo: {
-        notEmpty: true,
-        errorMessage: "Id não especificado",
-      },
-    }),
-  deleteFuncionario: checkSchema({
-    email: {
-      notEmpty: true,
-      errorMessage: "E-mail não especificado",
     },
   }),
   
