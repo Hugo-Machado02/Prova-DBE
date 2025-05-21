33f35ae28c4ab5ed7c2d1a6e375707653cf3bf11
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-21 12:18:28 -0300

    :wastebasket: remove: Exclui métodos desnecessários

--- src/repositories/funcionarioRepository.js
+++ src/repositories/funcionarioRepository.js
@@ -1,30 +1,17 @@
+const FuncionarioModel = require("../models/Funcionario");
 class FuncionarioRepository {
     async getAll() {
-      return await FuncionarioModel.find();
+        return await FuncionarioModel.find();
     }
-  
+
     async getByEmail(email) {
-      return await FuncionarioModel.findOne({ email });
+        return await FuncionarioModel.findOne({ email });
     }
-  
-    async getBasicInfo() {
-      return await FuncionarioModel.find().select('nome cargo');
-    }
-  
+
     async create(data) {
-      const novoFuncionario = new FuncionarioModel(data);
-      return await novoFuncionario.save();
-    }
-  
-    async updateByEmail(email, updateData) {
-      return await FuncionarioModel.findOneAndUpdate(
-        { email },
-        { $set: updateData },
-        { new: true }
-      );
-    }
-  
-    async deleteByEmail(email) {
-      return await FuncionarioModel.findOneAndDelete({ email });
+        const novoFuncionario = new FuncionarioModel(data);
+        return await novoFuncionario.save();
     }
 }
+
+module.exports = new FuncionarioRepository();
