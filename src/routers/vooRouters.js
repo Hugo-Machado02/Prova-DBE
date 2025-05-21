586e92db0fde07ab643bf9b5e7c8ed043437cc3a
Hugo Machado <hugo_machado02@hotmail.com>
2025-05-21 16:43:27 -0300

    ✨ feat: Implementa as rotas privadas

--- src/routers/vooRouters.js
+++ src/routers/vooRouters.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const vooController = require("../controllers/vooController");
 const vooValidator = require("../validator/vooValidator")
+const { authMiddleware, adminValidate } = require("../middleware/authMiddleware")
 
 router.get("/voos", vooController.getVoo);
-router.post("/voos/add", vooValidator.addVoo, vooController.addVoo);
+router.post("/voos/add", authMiddleware, adminValidate, vooValidator.addVoo, vooController.addVoo);
 router.get("/voos/hoje", vooController.getVooHoje);
-router.put("/voos/edicao", vooValidator.editVoo, vooController.editVoo);
+router.put("/voos/edicao", authMiddleware, vooValidator.editVoo, vooController.editVoo);
 router.delete("/voos/del", vooValidator.deleteVoo, vooController.deleteVoo);
 
 module.exports = router;
