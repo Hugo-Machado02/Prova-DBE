f6c6442b703a7dc678e6269442b0444037bfd433
GarotosDePr0grama <shayrakellyeleoterio@gmail.com>
2025-05-20 23:53:25 -0300

    :sparkles: feat: Configura Rotas dos Funcionários

--- src/routers/funcionarioRouters.js
+++ src/routers/funcionarioRouters.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const FuncionarioController = require();
+const funcionarioController = require("../controllers/funcionarioController");
+const funcionarioValidator = require("../validator/funcionarioValidator")
 
-router.get("/funcionarios", FuncionarioController);
+router.get("/funcionarios", funcionarioController.getFuncionarios);
+router.post("/funcionarios/add", funcionarioValidator.addFuncionario, funcionarioController.addFuncionario);
+router.put("/funcionarios/edicao", funcionarioValidator.editFuncionario, funcionarioController.editFuncionario);
+router.delete("/funcionarios/del", funcionarioValidator.deleteFuncionario, funcionarioController.deleteFuncionario);
 
 module.exports = router;
